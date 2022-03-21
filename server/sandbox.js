98fad3434c32daf666035dd7edc73d8750e00840
BENYEMNA Hamza <96919179+BENYEMNA-Hamza-DIA@users.noreply.github.com>
2022-03-21 18:10:32 +0100

    scrapping of all products

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,14 +1,123 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+/** Link of the brands :
+ * 
+ * https://adresse.paris/630-toute-la-collection
+ * https://www.dedicatedbrand.com
+ * https://www.montlimart.com/toute-la-collection.html?limit=all
+ * 
+ */
+
+const fetch = require("node-fetch");
+const fs = require('fs');
+
+/** Sources:
+ */
+
+const source_adresse = require('./sources/adresse');
+const source_dedicatedbrand = require('./sources/dedicatedbrand');
+const source_montlimart = require('./sources/montlimart');
+
+async function sandbox () {
   try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+    /**
+     * Scrapping for adresse
+     */
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const eshop_adresse = 'https://adresse.paris/630-toute-la-collection?id_category=630&n=118'
+    
+    console.log(`🕵️‍♀️  browsing ${eshop_adresse} source`);
+    
+    const total_products_adresse = [] 
 
-    console.log(products);
-    console.log('done');
+    let products_adresse = await source_adresse.scrape(eshop_adresse);
+    console.log(products_adresse);
+    products_adresse.forEach(product => total_products_adresse.push(product));
+
+    console.log('Scrapping was sucessfully done for adresse');
+    console.log(products_adresse);
+
+    
+    /**
+     * Scrapping dor dedicatedbrand
+     */
+
+     const eshop_dedicatedbrand = 'https://adresse.paris/630-toute-la-collection?id_category=630&n=118'
+     console.log(`🕵️‍♀️  browsing ${eshop_dedicatedbrand} source`);
+    
+     let total_products_scrapped_db = 0
+     let total_pages_scrapped_db = 0
+ 
+     const total_products_db = []
+     
+     for (var i = 1; ; i++){
+       total_pages_scrapped_db += 1
+       let eshop_dedicatedbrand = 'https://www.dedicatedbrand.com/en/men/all-men?p=' + i;
+       console.log(`🕵️‍♀️  browsing ${eshop_dedicatedbrand} source`);
+ 
+       let products_dedicatedbrand = await source_dedicatedbrand.scrape(eshop_dedicatedbrand);
+       if (products_dedicatedbrand.length==0){
+         
+         if (total_products_scrapped_db == total_products_db.length){
+          console.log('Scrapping was sucessfully done for dedicatedbrand');
+         }
+         else
+         {
+           console.log('Scrapping error')
+         }
+         break;
+       }
+       else{
+        console.log(products_dedicatedbrand);
+        products_dedicatedbrand.forEach(product => total_products_db.push(product));
+        console.log(products_dedicatedbrand.length,'products was scrapped from page',total_pages_scrapped_db);
+        total_products_scrapped_db += products_dedicatedbrand.length
+        }
+      }
+
+    /**
+     * Scrapping for montlimart
+     */
+     const eshop_montlimart = 'https://www.montlimart.com/toute-la-collection.html?limit=all'
+     console.log(`🕵️‍♀️  browsing ${eshop_montlimart} source`);
+     
+     const total_products_ml = [] 
+ 
+     let products_montlimart = await source_montlimart.scrape(eshop_montlimart);
+     console.log(products_montlimart);
+
+     products_montlimart.forEach(product => total_products_ml.push(product));
+     
+     console.log('Scrapping was sucessfully done for montlimart');
+
+    /**
+     * Storage of all products
+     */
+
+    const total_products = []
+    total_products_adresse.forEach(product => total_products.push(product));
+    total_products_db.forEach(product => total_products.push(product));
+    total_products_ml.forEach(product => total_products.push(product));
+    
+    console.log(total_products_adresse.length,'total products scrapped for adresse')
+    console.log(total_products_scrapped_db,'total products scrapped from',total_pages_scrapped_db,'pages');
+    console.log(total_products_ml.length,'total products scrapped for montlimart')
+
+    console.log(total_products.length,'total products scrapped')
+    console.log('Scrapping was sucessfully done');
+    /**
+     * Write a json file with all the products
+     */
+
+     const data = JSON.stringify(total_products);
+
+     fs.writeFileSync('total_products.json', data, (err) => {
+         if (err) {
+             throw err;
+         }
+         
+     });
+    console.log("JSON file 'total_products.json' is created and saved.");
     process.exit(0);
   } catch (e) {
     console.error(e);
