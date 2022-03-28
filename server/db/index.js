0e3ad3b7f97ab0c8974f5213a4f9273dc5da9954
BENYEMNA Hamza <96919179+BENYEMNA-Hamza-DIA@users.noreply.github.com>
2022-03-28 17:35:21 +0200

    connection to mongodb 2.0

--- server/db/index.js
+++ server/db/index.js
@@ -1,42 +1,79 @@
-// tuto : https://dev.to/dalalrohit/how-to-connect-to-mongodb-atlas-using-node-js-k9i
-// JavaScript source code
-const MongoClient = require('mongodb').MongoClient;
-//const { MongoClient } = require('mongodb');
-const MONGODB_URI = 'mongodb+srv://hamza-ben:uKCd4vwXye2SCuzS@clearfashion.so4t2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const MONGODB_DB_NAME = 'ClearFashion'
+const {MongoClient, ExplainVerbosity} = require('mongodb');
+const MONGODB_URI = 'mongodb+srv://hamza-ben:uKCd4vwXye2SCuzS@clearfashion.so4t2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGODB_DB_NAME = 'ClearFashion';
 
-const connectionParams = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
+let client;
+let db;
 
-async function connect() {
-    try {
-        const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-        let connexion_db=client.db(MONGODB_DB_NAME)
-        console.log('Successfully connected to database !')
-        return connexion_db
-    }
-    catch (err) {
-        console.error(`Error connecting to the database... \n${err}`);
-    }
+const products = require('../total_products.json');
 
-}
-
-connect();
 
 /**
- * Creating a collection with all the products
+ * Connection to the db 
+ * @param {*} MONGODB_URI 
+ * @param {*} MONGODB_DB_NAME 
  */
+const connect = async (uri = MONGODB_URI, name = MONGODB_DB_NAME) => { 
+    console.log("⏳ Connection to ClearFashion cluster ...");
+    try {
 
-const all_products = require('../total_products.json');
-
-async function create_collection(products) {
-    const db = await connect();
-    const collection = db.collection('all_products');
-    for (product of products) {
-        const result = collection.insertMany(product);
+        client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+        console.log("🎯 Connection Successful !");
+        db =  client.db(MONGODB_DB_NAME);
+    }
+    catch (error) {
+        console.log('Connection failed.', error);
     }
 }
 
-create_collection([all_products]);
+
+/**
+ * Close the connection to the db
+ */
+ const close = async () => {
+  await client.close();
+  console.log("Connection closed.");
+}
+
+
+/**
+* Create a database in the cluster
+*/
+
+
+async function create_database(){
+
+  const collection = db.collection('all_products');
+  const result = await collection.insertMany(products, {'ordered': false});
+  console.log("👕 Products successfully loaded in ClearFashion ");
+
+}
+
+/**
+ * Drop the database
+ */
+
+async function drop_database(){
+  await db.collection('all_products').drop();
+  console.log('Database dropped.')
+}
+  
+
+
+
+/**
+* Main function
+*/
+async function main(){
+  await connect();
+  
+  //Create database
+  //await create_database();
+
+  //Delete database
+  //await drop_database();
+
+  await close();
+};
+
+main();
