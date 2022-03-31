c590ec5f4186241c7f1ce523c96c5faf1096c100
BENYEMNA Hamza <96919179+BENYEMNA-Hamza-DIA@users.noreply.github.com>
2022-03-31 14:02:55 +0200

    search api

--- server/api.js
+++ server/api.js
@@ -56,11 +56,36 @@ console.log(`📡 Running on port ${PORT}`);
  * URL : http://localhost:8092/products/624578c70893460e53714433
  */
 
-
  app.get('/products/:_id', async (request, response) => {
   await connection();
   let product = await db.by_id(request.params._id);
   response.send({"product by id": product})
+})
+
+/**
+ * Search
+ */
+
+ app.get('/products/search', async (request, response) => {
+  // set default values for query parameters
+  const { brand = 'all', price = 'all', limit = 12, skip = 0, sort = 1 } = request.query;
+  if (brand === 'all' && price === 'all') {
+      const products = await db.find_limit([{ '$sort': { "price": parseInt(sort) } }, { '$limit': parseInt(limit) }, { '$skip': parseInt(skip) }]);
+      response.send(products);
+  } else if (brand === 'all') {
+      const products = await db.find_limit([{ '$match': { 'price': { '$lte': parseInt(price) } } }, { '$sort': { "price": parseInt(sort) } }, { '$limit': parseInt(limit) }, { '$skip': parseInt(skip) }]);
+      response.send(products);
+  } else if (price === 'all') {
+      const products = await db.find_limit([{
+          '$match': { 'brand': brand }
+      }, { '$sort': { "price": parseInt(sort) } }, { '$limit': parseInt(limit) }, { '$skip': parseInt(skip) }]);
+      response.send(products);
+  } else {
+      const products = await db.find_limit([{ '$match': { 'brand': brand } },
+      { '$match': { 'price': { '$lte': parseInt(price) } } },
+      { '$sort': { "price": parseInt(sort) } }, { '$limit': parseInt(limit) }, { '$skip': parseInt(skip) }]);
+      response.send(products);
+  }
 })
 
 
